9a4b2ae9d8bfd9a3d119321272d7b974dd212ab0
Omer Avraham <omeravraham10@gmail.com>
2024-08-26 12:11:11 +0000

    adding files

--- terraform/ec2/ec2.tf
+++ terraform/ec2/ec2.tf
@@ -2,20 +2,19 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-remote-state-ec2-test"
-    key    = "ec2/terraform.tfstate"
-    region = "eu-central-1"
+# EC2 instance creation
+resource "aws_instance" "example" {
+  ami           = "ami-0d1ddd83282187d18"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-044d911bc82994ac6"  # Specify the Subnet ID
+
+  tags = {
+    Name = "Test-EC2-Instance"
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0d1ddd83282187d18"  # Example AMI (replace with your preferred AMI)
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "TestEC2"
-  }
+# Output the public IP
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
 }
 
